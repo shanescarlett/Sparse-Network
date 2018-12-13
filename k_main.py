6364eb69a10b820c04eae2eab62dc93438bee6b3
Shane Scarlett <shane.choo+github@gmail.com>
2018-12-13 14:03:24 +0900

    test

--- k_main.py
+++ k_main.py
@@ -1,5 +1,4 @@
 import keras
-import k_model
 import numpy as np
 import gc
 import pickle
@@ -36,7 +35,7 @@ def getData():
 
 def trainModel(savePath):
 	xTrain, yTrain, xTest, yTest, inputShape = getData()
-	model = k_model.getModel(inputShape)
+	model = getModel(inputShape)
 	model.summary()
 	checkpoint = keras.callbacks.ModelCheckpoint(savePath, monitor = 'val_acc', verbose = 1, save_best_only = True,
 	                                             mode = 'max')
@@ -64,70 +63,86 @@ def evaluateCreatures(creaturePool, x, y):
 		individual.evaluateFitness(x, y)
 
 
-
+def getModel(inputShape):
+	L = keras.layers.Input(inputShape)
+	inputLayer = L
+	L = keras.layers.Conv2D(32, (3, 3), activation = 'relu')(L)
+	L = keras.layers.MaxPooling2D(2, 2)(L)
+	L = keras.layers.Dropout(0.2)(L)
+	L = keras.layers.Conv2D(64, (3, 3), activation = 'relu')(L)
+	L = keras.layers.MaxPooling2D(2, 2)(L)
+	L = keras.layers.Dropout(0.2)(L)
+	L = keras.layers.Flatten()(L)
+	L = keras.layers.Dense(128, activation = 'relu')(L)
+	L = keras.layers.Dropout(0.2)(L)
+	L = keras.layers.Dense(10, activation = 'softmax')(L)
+	outputLayer = L
+	model = keras.models.Model(inputLayer, outputLayer)
+	model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['acc'])
+	return model
 
 
 def main():
 	import creature
 	xTrain, yTrain, xTest, yTest, inputShape = getData()
 
-	# trainModel('model.h5')
-	model = loadModel('model.h5')
-	model.summary()
-
-	weights = model.get_weights()
-	print(model.evaluate(xTest, yTest))
-	population = 0
-	generations = 0
-	elitismRatio = 0.4
-	mutationRate = 0.01
-	creaturePool = []
-	for i in range(population):
-		print('Creating Creature ' + str(i))
-		individual = creature.Creature(inputShape, weights)
-		individual.pruneWeights()
-		individual.evaluateFitness(xTest, yTest)
-		creaturePool.append(individual)
-		if i%10 == 0:
-			keras.backend.clear_session()
-
-
-	# evaluateCreatures(creaturePool, xTest, yTest)
-
-	progress = []
-
-	for gen in range(generations):
-		print('Generation ' + str(gen))
-		print('Max prev: ' + str(creaturePool[0].getFitness()))
-		progress.append(creaturePool[0].getFitness())
-		creaturePool.sort(key=lambda x: x.fitness, reverse=True)
-		keptIndex = round(elitismRatio * population)
-		for i in range(keptIndex):
-			mateIndex = i
-			while mateIndex == i:
-				mateIndex = np.random.random_integers(0, keptIndex - 1)
-			print('Mating creature ' + str(i) + ' and ' + str(mateIndex) + ' to ' + str(i + keptIndex))
-			childCreature = creaturePool[i].crossover(creaturePool[mateIndex], mutationRate)
-			childCreature.pruneWeights()
-			childCreature.evaluateFitness(xTest, yTest)
-			creaturePool[i + keptIndex] = childCreature
-			if i > 0:
-				creaturePool[i].mutate(mutationRate)
-			if i % 10 == 0:
-				keras.backend.clear_session()
-		for i in range(keptIndex * 2, population):
-			print('Creating Creature ' + str(i))
-			newCreature = creature.Creature(inputShape, weights)
-			newCreature.pruneWeights()
-			newCreature.evaluateFitness(xTest, yTest)
-			creaturePool[i] = newCreature
-			if i % 10 == 0:
-				keras.backend.clear_session()
-		gc.collect()
-		#keras.backend.clear_session()
-		with open('best.creature', 'wb') as f:
-			pickle.dump(creaturePool[0].prob, f)
-		np.savetxt('progress.csv', np.asarray(progress), fmt = '%f', encoding = 'utf8')
+	trainModel('model.h5')
+	# model = loadModel('model.h5')
+	# model.summary()
+	#
+	# weights = model.get_weights()
+	# print(model.evaluate(xTest, yTest))
+	# population = 0
+	# generations = 0
+	# elitismRatio = 0.4
+	# mutationRate = 0.01
+	# creaturePool = []
+	# for i in range(population):
+	# 	print('Creating Creature ' + str(i))
+	# 	individual = creature.Creature(inputShape, weights)
+	# 	individual.pruneWeights()
+	# 	individual.evaluateFitness(xTest, yTest)
+	# 	creaturePool.append(individual)
+	# 	if i%10 == 0:
+	# 		keras.backend.clear_session()
+	#
+	#
+	# # evaluateCreatures(creaturePool, xTest, yTest)
+	#
+	# progress = []
+	#
+	# for gen in range(generations):
+	# 	print('Generation ' + str(gen))
+	# 	print('Max prev: ' + str(creaturePool[0].getFitness()))
+	# 	progress.append(creaturePool[0].getFitness())
+	# 	creaturePool.sort(key=lambda x: x.fitness, reverse=True)
+	# 	keptIndex = round(elitismRatio * population)
+	# 	for i in range(keptIndex):
+	# 		mateIndex = i
+	# 		while mateIndex == i:
+	# 			mateIndex = np.random.random_integers(0, keptIndex - 1)
+	# 		print('Mating creature ' + str(i) + ' and ' + str(mateIndex) + ' to ' + str(i + keptIndex))
+	# 		childCreature = creaturePool[i].crossover(creaturePool[mateIndex], mutationRate)
+	# 		childCreature.pruneWeights()
+	# 		childCreature.evaluateFitness(xTest, yTest)
+	# 		creaturePool[i + keptIndex] = childCreature
+	# 		if i > 0:
+	# 			creaturePool[i].mutate(mutationRate)
+	# 		if i % 10 == 0:
+	# 			keras.backend.clear_session()
+	# 	for i in range(keptIndex * 2, population):
+	# 		print('Creating Creature ' + str(i))
+	# 		newCreature = creature.Creature(inputShape, weights)
+	# 		newCreature.pruneWeights()
+	# 		newCreature.evaluateFitness(xTest, yTest)
+	# 		creaturePool[i] = newCreature
+	# 		if i % 10 == 0:
+	# 			keras.backend.clear_session()
+	# 	gc.collect()
+	# 	#keras.backend.clear_session()
+	# 	with open('best.creature', 'wb') as f:
+	# 		pickle.dump(creaturePool[0].prob, f)
+	# 	np.savetxt('progress.csv', np.asarray(progress), fmt = '%f', encoding = 'utf8')
 	print("End")
 
 
